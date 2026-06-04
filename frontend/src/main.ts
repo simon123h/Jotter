57d78e9d6746577b564544b0766c735eb0fcf51e
simon123h <simon123h@users.noreply.github.com>
2026-06-04 12:50:11 +0200

    feat(frontend): turn application into installable PWA

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,3 +8,16 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.mount('#app');
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((reg) => {
+        console.log('ServiceWorker registered with scope: ', reg.scope);
+      })
+      .catch((err) => {
+        console.error('ServiceWorker registration failed: ', err);
+      });
+  });
+}
